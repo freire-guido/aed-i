8836ffd85cfa031bdf7a558c17210f67a562b5b7
freire-guido <freireguido@outlook.com>
2021-10-30 21:45:59 -0300

    implementar insertarOrdenado y perteneceBinario

--- auxiliares.h
+++ auxiliares.h
@@ -1,7 +1,14 @@
 #ifndef SOLUCION_AUXILIARES_H
 #define SOLUCION_AUXILIARES_H
 
+#include "ejercicios.h"
+
+//Dada una lista v de T ordenados, devuelve verdadero si e pertenece a v
 template<typename T>
-bool perteneceBinario(vector<T> v, T e);
+bool perteneceBinario(T e, const vector<T>& v);
+template<typename T>
+void insertarOrdenado(T e, vector<T>& v);
+
+#include "auxiliares.hpp"
 
 #endif //SOLUCION_AUXILIARES_H
---
+++ auxiliares.hpp
@@ -0,0 +1,39 @@
+#include "definiciones.h"
+#include "auxiliares.h"
+#include "gtest/gtest.h"
+#include "ejercicios.h"
+
+//Dada una lista v de T ordenados, devuelve verdadero si e pertenece a v
+template<typename T>
+bool perteneceBinario(T e, const vector<T>& v){
+    int i = 0;
+    int j = v.size() - 1;
+    while (i <= j){  
+        int k = (i + j)/2;
+        if(v[k] == e){
+            return true;
+        }
+        else if(v[k] < e){
+            i = k + 1;
+        } else {
+            j = k - 1;
+        }
+    }
+    return false;
+}
+
+template<typename T>
+void insertarOrdenado(T e, vector<T>& v){
+    //busca el elemento "anterior" (el menor mas grande)
+    int i = 0;
+    int j = v.size() - 1;
+    while (i < j){
+        int k = (i + j) / 2;
+        if (v[k] <  e){
+            i = k + 1;
+        } else {
+            j = k - 1;
+        }
+    }
+    v.insert(v.begin() + i + 1, e);
+}
